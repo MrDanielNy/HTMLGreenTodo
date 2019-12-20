fddbda46cd1e0ff509bc5b9f67c691b1d8a4aee3
jonta052 <55479519+jonta052@users.noreply.github.com>
2019-12-20 08:23:16 +0100

    Ändrade CSS
    Tre filer ändrade i

--- main.js
+++ main.js
@@ -36,18 +36,21 @@ var DocumentHandler = (function() {
 
     function init() {
         TODOStorage.init();
-
+        var wrapper = "";
         var gottenTodo = TODOStorage.listTodos();
         var listOfTodos = document.getElementById("list-of-todos");
 
         for (var i = 0; i < gottenTodo.length; i++) {
             console.log(gottenTodo[i].activity.activityName);
-            if (gottenTodo[i].activity.done) {
-                listOfTodos.innerHTML += "<li> 🔵" + gottenTodo[i].activity.activityName + "<button class='done-button' data-id=" + gottenTodo[i].id + "> Ej klar</button><button class='delete-button' data-id=" + gottenTodo[i].id + ">Ta bort</button>";
-            } else {
-                listOfTodos.innerHTML += "<li> 🔴" + gottenTodo[i].activity.activityName + "<button class='done-button' data-id=" + gottenTodo[i].id + ">Klar</button><button class='delete-button' data-id=" + gottenTodo[i].id + ">Ta bort</button>";
+            if (i%2==0) {
+                wrapper = "wrappereven"
             }
-
+            else{wrapper = "wrapperodd"}
+            if (gottenTodo[i].activity.done) {
+                listOfTodos.innerHTML += "<div id="+wrapper+"><div id="+"text"+"><li><span class="+"gdot"+"></span>" + gottenTodo[i].activity.activityName + "</div><div id="+"twobuttons"+"><button class='done-button' data-id=" + gottenTodo[i].id + "> Ej klar</button><button class='delete-button' data-id=" + gottenTodo[i].id + ">Ta bort</button><br></div></div>";
+            } else {
+                listOfTodos.innerHTML += "<div id="+wrapper+"><div id="+"text"+"><li><span class="+"rdot"+"></span>" + gottenTodo[i].activity.activityName + "</div><div id="+"twobuttons"+"><button class='done-button' data-id=" + gottenTodo[i].id + ">Klar</button><button class='delete-button' data-id=" + gottenTodo[i].id + ">Ta bort</button><br></div></div>";
+            }
         }
 
         var addButton = document.getElementById("add").addEventListener("click", function() {
